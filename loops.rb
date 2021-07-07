8b276845b0b950d9086a79378054dd92680193ad
Jahanzaib Malik <jahanzaib.malik@pakeventures.com>
2021-07-07 18:28:20 +0500

    "Topics learnt till 07/07/2021"

--- loops.rb
+++ loops.rb
@@ -1,5 +1,32 @@
-for i in 2..100
+for i in 2..10
     if i.even?
         puts "#{i} is even"
     end
+end
+
+
+arr = ["pak","wheels",2,5,76]
+
+for x in arr
+    puts "x is: #{x} and its type is: #{x.class}"
+end
+
+
+i = 3
+while i < 25
+    puts "#{i} is smoll"
+    i+=1
+    if i==10
+        next
+    end
+end
+
+i.times{
+    puts "#{i}-Hello world"
+}
+
+#eachhhhhhh
+
+arr.each do | e |
+    puts e.class
 end
---
+++ person.rb
@@ -0,0 +1,14 @@
+class Person
+    def talk()
+        puts "Hello i am talking to you"
+    end
+
+    def run()
+        puts "Hello i am running"
+    end
+end
+
+
+p = Person.new
+
+p.talk
